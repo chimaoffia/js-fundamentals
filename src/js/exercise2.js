8b107daf2781dae59b0cca26ed569ecab3c6d5f0
chimaoffia <offiahchima@gmail.com>
2022-09-03 22:49:15 +0100

    working on arrays of objects and loop

---
+++ src/js/exercise2.js
@@ -0,0 +1,66 @@
+// const car = {
+//     carName: 'Toyota',
+//     carModel: 'Camry Hybrid',
+//     makerYear: 2008,
+//     carColour: ['Red', 'black', 'white'],
+
+//     hasDrivingLicence: function () {
+//         return 'yes i have a driving licence'
+//     },
+
+//     owner: function() {
+//         return `yes i own ${this.carName}`
+//     }
+// }
+
+
+// console.log(car.carName)
+// console.log(car.carColour)
+// console.log(car.hasDrivingLicence())
+// console.log(car.owner())
+
+
+
+const friends = ['Tochi', 'mike', 'collins', 'chima', 'chuks'];
+
+for(i = 0; i < friends.length; i++) {
+//    console.log(friends[i])
+    if(friends[i] === 'collins') {
+        console.log(`${friends[i]} you are scheduled for interview on monday.`)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const staffs = [
+    'Christian', 'Elijah', 'Gideon', 'Alex', 'Franka','Aziz',
+'Jessica', 'Rose', 'Naomi', 'Lovina', 'Chidera'
+];
+
+for(i = 0; i < staffs.length; i++) {
+    // console.log(staffs[i])
+    if(staffs[i] === 'christian') {
+        console.log(`${staffs[i]} na man you be.`)
+    }
+}
--- src/js/loop.js
+++ src/js/loop.js
@@ -1,3 +1,47 @@
 for(let i = 0; i < 5; i++) {
     console.log(i, 'hello world')
 }
+
+
+const cars = ['Benz', 'Toyota', 'Honda'];
+
+for (let i = 0; i < cars.length; i++){
+    console.log(cars[i])
+}
+
+const myStudents = [
+    {firstName: 'chima', lastName: 'offia', scores: 79},
+    {firstName: 'david', lastName: 'james', scores: 60},
+    {firstName: 'christian', lastName: 'nnamani', scores: 80},
+    {firstName: 'grace', lastName: 'igwe', scores: 50},
+]
+
+for(let i=0; i < myStudents.length; i++) {
+     console.log(`${myStudents[i].firstName} scored ${myStudents[i].scores}`);
+}
+
+
+let total =0
+for(let i=0; i < myStudents.length; i++) {
+    let student = myStudents[i] 
+    total += student.scores
+}
+
+
+console.log(`the total scores is ${total}.`)
+
+
+
+let average = Math.floor(total/myStudents.length);
+console.log(average)
+
+
+const data = function (arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let student = arr[i]
+        console.log(`${student.firstName} scored ${student.scores}`)
+    }
+}
+
+
+data(myStudents)
